0fa8ce339d18bdc6a3dce53348c0df7be54d704f
William Moolman <william.moolman@hackpartners.com>
2020-04-26 17:40:24 +0100

    Added density var

--- scripts/Planet.gd
+++ scripts/Planet.gd
@@ -5,13 +5,14 @@ extends RigidBody
 # var a = 2
 # var b = "text"
 export var PLANET_SCALE = 1
+export var PLANET_DENSITY = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	scale = Vector3(PLANET_SCALE,PLANET_SCALE,PLANET_SCALE)
 	$MeshInstance.scale = scale
 	$CollisionShape.scale = scale
-	mass = mass * pow(PLANET_SCALE,3)
+	mass = mass * pow(PLANET_SCALE,3) * PLANET_DENSITY
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
